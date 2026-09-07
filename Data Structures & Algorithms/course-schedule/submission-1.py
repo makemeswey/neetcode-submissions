07933445ddf303e729b027b40acc999d1b5f503d
Swetha Jayaprasad Rao Ethamukkala <68841698+makemeswey@users.noreply.github.com>
2026-09-07 14:15:12 +0800

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,25 @@
+from collections import defaultdict, deque
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        indegree = [0] * numCourses
+
+        for u, v in prerequisites:
+            graph[v].append(u)
+            indegree[u] += 1
+
+        queue = deque([n for n in range(numCourses) if indegree[n] == 0])
+
+        order = []
+        while queue:
+            node = queue.popleft()
+            order.append(node)
+
+            for neighbor in graph[node]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+
+        return len(order) == numCourses
+        
