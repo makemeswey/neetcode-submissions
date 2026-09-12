d91810908c5274dcdeb915c520e13359c06991ea
Swetha Jayaprasad Rao Ethamukkala <68841698+makemeswey@users.noreply.github.com>
2026-09-12 16:33:31 +0800

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        mapping = {
+            ")": "(",
+            "}": "{",
+            "]":"["
+        }
+
+        stack = []
+
+        for ch in s:
+            if ch in mapping:
+                if stack and mapping[ch] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(ch)
+
+        return len(stack) == 0
